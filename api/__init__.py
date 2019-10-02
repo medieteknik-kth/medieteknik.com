2cb240278346901d56d01616dccb5f6bbed2a192
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2019-10-02 21:26:08 +0200

    Ändrade för att stämma överens med Docker

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://medieteknik:rgb@db/medieteknikdev'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DB_CONNECT_STR"]
 db = SQLAlchemy(app)
 
 @app.route("/create_all")
