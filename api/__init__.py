3373548f693ce57ce9eddd0133ab7224c3591d4c
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2019-10-18 20:55:57 +0200

    CAS test

--- api/__init__.py
+++ api/__init__.py
@@ -1,15 +1,24 @@
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_cas import CAS
+from flask_cas import CAS, login_required
 
 import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DB_CONNECT_STR"]
+app.config['SECRET_KEY'] = os.environ["FLASK_SECRET_KEY"]
+app.config['CAS_SERVER'] = os.environ["CAS_SERVER"]
+
 db = SQLAlchemy(app)
 CORS(app)
+CAS(app)
+
+@app.route("/authtest")
+@login_required
+def auth_test():
+    return "Du är inloggad som " + str(session["CAS_USERNAME"]) + ". CAS_ATTRIBUTES=" + str(session["CAS_ATTRIBUTES"])
 
 @app.route("/create_all")
 def route_create_all():
@@ -48,5 +57,7 @@ def route_create_all():
     db.session.commit()
 
     return "klar"
+
+
 
 from api import routes
