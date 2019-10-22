46db6410fa3fb8b1e10439b507153904ecd23293
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2019-10-22 14:03:16 +0200

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, session, url_for, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_cas import CAS, login_required
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://medieteknik:rgb@db/medieteknikdev'
 db = SQLAlchemy(app)
 CORS(app)
+
+if os.getenv("LOCAL_CAS") == "1":
+    local_cas = Blueprint("cas", "cas")
+    @local_cas.route("/login")
+    def login():
+        session["CAS_USERNAME"] = "joppe"
+        session["CAS_ATTRIBUTES"] = ""
+        return "hej"
+    @local_cas.route("/logout")
+    def logout():
+        del session["CAS_USERNAME"]
+        del session["CAS_ATTRIBUTES"]
+        return "hejdå"
+
+    app.register_blueprint(local_cas)
+else:
+    CAS(app)
+
+@app.route("/authtest")
+@login_required
+def auth_test():
+    return "Du är inloggad som " + str(session["CAS_USERNAME"]) + ". CAS_ATTRIBUTES=" + str(session["CAS_ATTRIBUTES"])
 
 @app.route("/create_all")
 def route_create_all():
