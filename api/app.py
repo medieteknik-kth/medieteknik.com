ce066f3bd2bf1901f0111a6fb99fb1cfc7182676
Fredrik Lundkvist <flundkvi@kth.se>
2019-10-30 10:19:13 +0100

    stashissues

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from flask import (
     jsonify,
     request,
 )
-from app.documents.document_functions import save_documents, get_documents
+from documents.document_functions import save_documents, get_documents
 
 app = Flask(__name__)
 
@@ -18,11 +18,11 @@ def placeholder():
 #Endpoint för dokument
 @app.route('/api/documents', methods=['GET', 'POST'])
 def document_endpoint():
-    if request.method == 'GET':
+    if request.method == 'POST':
         if request.files is None:
-            return jsonify(status=422, status="no files attached")
+            return jsonify(status=422, message="no files attached")
         else:
             save_documents(request.files)
-    if request.method == 'POST':
+    if request.method == 'GET':
         documents = get_documents()
         return jsonify(status = 200, documents=documents)
