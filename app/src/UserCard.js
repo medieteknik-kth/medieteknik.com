c49f5ccc3214aa1f1ca0dd60c058c5c26cdb3899
agyllang <46810941+agyllang@users.noreply.github.com>
2019-10-30 07:48:22 +0100

    userform
    flera infyllnads alternativ

--- app/src/UserCard.js
+++ app/src/UserCard.js
@@ -7,20 +7,27 @@ class UserCard extends React.Component {
     this.state = { user: {} };
   }
 
-    componentDidMount() {
-        fetch("/api/user/" + this.props.id).then(response => response.json())
-        .then(data => {
-            this.setState({user: data});
-        });
-    }
-    render() {
-        return <div class="userCard">
-            <img class="userImage" src={this.state.user.profile_picture} alt="Profile Picture" />
-            <div class="userCardBanner">
-                <UserCardTextbox user={this.state.user} />
-            </div>
-        </div>;
-    }
+  componentDidMount() {
+    fetch("/api/user/" + this.props.id)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ user: data });
+      });
+  }
+  render() {
+    return (
+      <div class="userCard">
+        <img
+          class="userImage"
+          src={this.state.user.profile_picture}
+          alt="Profile Picture"
+        />
+        <div class="userCardBanner">
+          <UserCardTextbox user={this.state.user} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default UserCard;
--- app/src/UserForm.js
+++ app/src/UserForm.js
@@ -1,30 +1,51 @@
-import React from 'react'; 
+import React from "react";
 
 class UserForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {username: ''};
+  constructor(props) {
+    super(props);
+    this.state = { username: "" };
+  }
+  render() {
+    let header = "";
+    if (this.state.username) {
+      header = <h1>Redigera din profil</h1>;
+    } else {
+      header = "";
     }
-    render() {
-        let header = '';
-        if (this.state.username) {
-            header = <h1>Redigera din profil</h1> 
-        }  else { 
-            header = '';
-        }
-        return (
-            <form>
-                {header}
-                <p>Namn: </p>
-                <input 
-                type='text'
-                onChange={this.myChangeHandler}
-                />
-            </form>
-        );
-    }
+    return (
+      <form>
+        {header}
+        <p>Namn: </p>
+        <input type="text" onChange={this.myChangeHandler} placeholder="Namn" />
+        <p>Efternamn: </p>
+        <input
+          type="text"
+          onChange={this.myChangeHandler}
+          placeholder="Efternamn"
+        />
+        <p>Email: </p>
+        <input
+          type="text"
+          onChange={this.myChangeHandler}
+          placeholder="Email"
+        />
+        <p>Facebook: </p>
+        <input
+          type="text"
+          onChange={this.myChangeHandler}
+          placeholder="Facebook"
+        />
+        <p>Linkedin: </p>
+        <input
+          type="text"
+          onChange={this.myChangeHandler}
+          placeholder="Linkedin"
+        />
+      </form>
+    );
+  }
 }
 
 /* ReactDOM.render(<MyForm />, document.getElementById('root')); */
 
-export default UserForm; 
+export default UserForm;
