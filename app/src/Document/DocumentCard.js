a027966dc336028602bf2e55dcb60a053936eefb
Rasmus Rudling <rasmusrudling@gmail.com>
2019-11-18 09:33:44 +0100

    Ny dokument-branch

---
+++ app/src/Document/DocumentCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import DownloadBtn from './Download-btn.svg';
+
+
+class DocumentCard extends Component {
+    render() {
+
+        return (
+            <div className={'doc-cards'}>
+                <b>{this.props.doctype}</b>
+                <h2>{this.props.headingText}</h2>
+                <p>Uppladdat: {this.props.publishDate}</p>
+                <p>Av: {this.props.publisher}</p>
+
+                <div className = "download-btn-circle">
+                    <i className = "download-btn-arrow"></i>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DocumentCard;
