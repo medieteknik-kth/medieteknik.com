564227736f851fe96e9eab919b68b0ae3caab999
agyllang <46810941+agyllang@users.noreply.github.com>
2019-11-27 12:02:59 +0100

    design user input fields
    jobbat med o få all design att funka beroende på vilken input type="" är, samt gjort en design för när man markerar input field

--- app/src/ImageUpload.js
+++ app/src/ImageUpload.js
@@ -1,56 +1,58 @@
 // https://codepen.io/hartzis/pen/VvNGZP
 import React from "react";
+import "./componentsCSS/ImageUpload.css";
 
-class ImageUpload extends React.Component{
-    constructor(props) {
-      super(props);
-      this.state = {
-        file: '',
-        imagePreviewUrl: ''
-      };
-      this._handleImageChange = this._handleImageChange.bind(this);
-      this._handleSubmit = this._handleSubmit.bind(this);
-    }
-  
-    _handleSubmit(e) {
-      e.preventDefault();
-      // TODO: do something with -> this.state.file
-    }
-  
-    _handleImageChange(e) {
-      e.preventDefault();
-  
-      let reader = new FileReader();
-      let file = e.target.files[0];
-  
-      reader.onloadend = () => {
-        this.setState({
-          file: file,
-          imagePreviewUrl: reader.result
-        });
-      }
-  
-      reader.readAsDataURL(file)
-    }
-  
-    render() {
-      let {imagePreviewUrl} = this.state;
-      let $imagePreview = null;
-      if (imagePreviewUrl) {
-        $imagePreview = (<img src={imagePreviewUrl} />);
-      }
-  
-      return (
-        <div>
-          <form onSubmit={this._handleSubmit}>
-            <input type="file" onChange={this._handleImageChange} />
-            <button type="submit" onClick={this._handleSubmit}>Upload Image</button>
-          </form>
-          {$imagePreview}
-        </div>
-      )
-    }
-  
+class ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: "",
+      imagePreviewUrl: ""
+    };
+    this._handleImageChange = this._handleImageChange.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
   }
 
-  export default ImageUpload;
+  _handleSubmit(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  render() {
+    let { imagePreviewUrl } = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = <img src={imagePreviewUrl} />;
+    }
+
+    return (
+      <div className="ImageUploadContainer">
+        <form onSubmit={this._handleSubmit}>
+          <input type="file" onChange={this._handleImageChange} />
+          <button type="submit" onClick={this._handleSubmit}>
+            Upload Image
+          </button>
+        </form>
+        {$imagePreview}
+      </div>
+    );
+  }
+}
+
+export default ImageUpload;
