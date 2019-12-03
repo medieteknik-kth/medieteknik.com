c7b8c783b33a9a24d9e26f89f46d0dded22a7d63
Fredrik Lundkvist <flundkvi@kth.se>
2019-12-03 11:59:54 +0100

    Lade till fil för API-hantering

--- app/src/lib/fetch_from_api.js
+++ app/src/lib/fetch_from_api.js
@@ -1,13 +1,19 @@
 /**
- * Funktion för att hämta data från API:t
- * Importera och anropa den här funktionen i ComponentDidMount(), här är ett exempel:
- *  const data = fetchData('documents')
- *  this.setState({documents: data})
- * @param endpoint: api-endpointen du vill hämta data från
- * @returns den hämrade datan i JSON-format
+ * Klass för att hämta data från API:t
+ * Förhoppningsvis är funktionerna ganska obvious
  */
-export default fetchData = (endpoint) => {
-    return fetch(`${BASE_URL}/${endpoint}`).then(res => res.json());
-}
 
-const BASE_URL = "/api"
+const BASE_URL = "/api/"
+
+export default {
+    /**
+     * getItems: hämta data från API
+     * @param endpoint: Api-endpoint att hämta från, t.ex. "documents" OBS: behövs inga snedstreck!
+     * exempel på använding: API.getItems(documents).then(items => this.setState({loaded: items}))
+     */
+    getItems: (endpoint) => {
+        return fetch(BASE_URL + endpoint)
+          .then(res => res.json());
+      }
+
+}
