5d4cf484c506b7efe629c2dcb507f39d2c95fb3c
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2020-01-07 22:54:31 +0100

    Fix till förra commit

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -84,6 +84,7 @@ def auth_test():
 @app.route("/create_all")
 def route_create_all():
     from api.models.user import User, Committee, CommitteePost, relationship_table
+    from api.models.document import Document, Tag, DocumentTags
     db.drop_all()
     db.create_all()
 
