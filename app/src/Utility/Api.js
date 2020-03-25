38f3f1d6b9cf7f7c3b53b2e6df4c6acccdaa42b1
agyllang <46810941+agyllang@users.noreply.github.com>
2020-03-25 16:04:28 +0100

    OfficialsBoard
    refractor for new db code

--- app/src/Utility/Api.js
+++ app/src/Utility/Api.js
@@ -1,34 +1,40 @@
 function GetApiObject(resource) {
-  const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://api.medieteknik.com/' : 'http://localhost:5000/';
+  const API_BASE_URL =
+    process.env.NODE_ENV === "production"
+      ? "https://api.medieteknik.com/"
+      : "http://localhost:5000/";
   return {
     GetAll() {
-      return fetch(API_BASE_URL + resource).then((response) => response.json());
+      return fetch(API_BASE_URL + resource).then(response => response.json());
     },
     GetById(id) {
-      return fetch(`${API_BASE_URL}${resource}/${id}`).then((response) => response.json());
+      return fetch(`${API_BASE_URL}${resource}/${id}`).then(response =>
+        response.json()
+      );
     },
     Update(id, data) {
       return fetch(`${API_BASE_URL}${resource}/${id}`, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json"
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data)
       });
     },
     Create(data) {
       return fetch(API_BASE_URL + resource, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json"
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data)
       });
-    },
+    }
   };
 }
 
 export default {
-  Committees: GetApiObject('committees'),
-  Pages: GetApiObject('pages'),
+  Committees: GetApiObject("committees"),
+  Officials: GetApiObject("officials"),
+  Pages: GetApiObject("pages")
 };
