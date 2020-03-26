c4fa02bcd4803bab92b13b08b33ce08c21bf5666
agyllang <46810941+agyllang@users.noreply.github.com>
2020-03-26 11:21:44 +0100

    Poster presenteras nu med vilken typ av postkategori (studiesocialt, näringsliv och kom.. etc)

--- app/src/Components/OfficialsBoard/OfficialsBoard.jsx
+++ app/src/Components/OfficialsBoard/OfficialsBoard.jsx
@@ -6,23 +6,37 @@ import "./OfficialsBoard.css";
 
 export default function OfficialsBoard() {
   const [officials, setOfficials] = useState([]);
-
   useEffect(() => {
     Api.Officials.GetAll().then(data => {
       setOfficials(data);
     });
   }, []);
+  const official_title = [];
+
+  officials.map(official =>
+    !official_title.includes(official.post.category)
+      ? official_title.push(official.post.category)
+      : null
+  );
 
   return (
     <div>
-      {officials.map(official => (
-        <div className="userList">
-          <UserCard
-            key={official.user.id}
-            user={official.user}
-            subtitle={official.post.name}
-            email={official.post.email}
-          />
+      {official_title.map(title => (
+        <div>
+          <h3 className="officialHeader">{title}</h3>
+
+          <div className="userList">
+            {officials
+              .filter(official => official.post.category === title)
+              .map(official => (
+                <UserCard
+                  key={official.user.id}
+                  user={official.user}
+                  subtitle={official.post.name}
+                  email={official.post.email}
+                />
+              ))}
+          </div>
         </div>
       ))}
     </div>
