85a2cce79c4b9d24fc78fcd8d17acfd7d7c4d577
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2020-04-07 22:48:02 +0200

    Påbörjade backend för att redigera poster

--- api/api/resources/committee_post.py
+++ api/api/resources/committee_post.py
@@ -8,8 +8,20 @@ class CommitteePostResource(Resource):
         committee_post = CommitteePost.query.get(id)
         return jsonify(committee_post.to_dict())
 
+    def delete(self, id):
+        # Ta bort
+        pass
+    
+    def patch(self, id):
+        # Redigera
+        pass
+
 class CommitteePostListResource(Resource):
     def get(self):
         committee_posts = CommitteePost.query.all()
         data = [committee_post.to_dict() for committee_post in committee_posts]
         return jsonify(data)
+    
+    def put(self):
+        # Ny post
+        pass
