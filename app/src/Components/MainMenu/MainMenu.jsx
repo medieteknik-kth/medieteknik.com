8a8f8fde73020a29510a61a019ceb07202e040a5
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2020-06-23 18:23:13 +0200

    Ändrade namn på bokningar i menyn

--- app/src/Components/MainMenu/MainMenu.jsx
+++ app/src/Components/MainMenu/MainMenu.jsx
@@ -47,7 +47,7 @@ const PageWithMainMenu = (props) => {
         { title_sv: 'Styrelsen', title_en: 'The Board', link: '/styrelsen' },
         { title_sv: 'Dokument', title_en: 'Documents', link: '/documents' },
         { title_sv: 'Sektionsmedlemmar', title_en: 'Chapter Members', link: '/medlemmar' },
-        { title_sv: 'Bokningar', title_en: 'Booking', link: '/bokningar' },
+        { title_sv: 'Lokalbokning', title_en: 'Booking', link: '/bokningar' },
       ],
     },
     {
