c291af8a625e7617ea8ce0aedd4830ea98aecc5c
ellaklara <ellaklara.westerlund@gmail.com>
2020-07-02 15:23:33 +0200

    Update officials styling

--- app/src/Components/OfficialsBoard/OfficialsBoard.jsx
+++ app/src/Components/OfficialsBoard/OfficialsBoard.jsx
@@ -14,22 +14,22 @@ export default function OfficialsBoard() {
     '2019/2020',
     '2018/2019'
   ]
-  const [officials, setOfficials] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [operationalYear, setOperationalYear] = useState(operationalYears[0]);
+  const [officials, setOfficials] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [operationalYear, setOperationalYear] = useState(operationalYears[0])
 
   useEffect(() => {
-    setIsLoading(true);
+    setIsLoading(true)
     Api.Officials.GetWithParameters({'forOperationalYear' : operationalYear}).then((data) => {
       setOfficials(data)
-      setIsLoading(false);
+      setIsLoading(false)
     }).catch(err => {
       setIsLoading(false)
     });
-  }, [operationalYear]);
+  }, [operationalYear])
 
   //TODO: Add backend for post categories (weigth, email etc)
-  const categoryTitles = [];
+  const categoryTitles = []
   officials.map((official) =>
     !categoryTitles.includes(official.post.category)
       ? categoryTitles.push(official.post.category)
@@ -49,7 +49,7 @@ export default function OfficialsBoard() {
         </div>
 
         { isLoading ? 
-          <div className='official-center'>
+          <div className='officials-center'>
             <Spinner/>
           </div> :
           categoryTitles.length > 0 ? 
@@ -60,7 +60,7 @@ export default function OfficialsBoard() {
                   <p>medieteknik@medieteknik.com</p> {/*TODO: Add backend for post categories email*/}
                 </div>
 
-                <div className='user-list-official'>
+                <div className='officials-list'>
                   {officials
                     .filter((official) => official.post.category === title)
                     .map((official) => (
@@ -74,7 +74,7 @@ export default function OfficialsBoard() {
                 </div>
               </div>))
             : 
-              <div className='official-center'>
+              <div className='officials-center'>
                 <LocaleText phrase='officials/could_not_load'/>
               </div>
             }
