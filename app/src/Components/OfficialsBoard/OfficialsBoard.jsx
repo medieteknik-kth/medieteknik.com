39a693d3a382699988baaecb68dacbf47e8f928f
ellaklara <ellaklara.westerlund@gmail.com>
2020-07-02 14:42:21 +0200

    Update officials board

--- app/src/Components/OfficialsBoard/OfficialsBoard.jsx
+++ app/src/Components/OfficialsBoard/OfficialsBoard.jsx
@@ -1,17 +1,29 @@
-import React, { useState, useEffect } from "react";
-import UserCardOfficial from "../UserCard/UserCardOfficial";
-import Api from "../../Utility/Api";
+import React, { useState, useEffect } from 'react';
+import OfficialCard from './OfficialCard';
+import Api from '../../Utility/Api';
 
-import "./OfficialsBoard.css";
+import './OfficialsBoard.css';
+import Dropdown from '../Common/Form/Dropdown';
+import Spinner from '../Common/Spinner/Spinner';
+import { LocaleText } from '../../Contexts/LocaleContext';
 
 export default function OfficialsBoard() {
+
+  const operationalYears = [
+    '2019/2020',
+    '2018/2019'
+  ]
   const [officials, setOfficials] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [operationalYear, setOperationalYear] = useState(operationalYears[0]);
 
   useEffect(() => {
-    Api.Officials.GetAll().then((data) => {
-      setOfficials(data);
+    setIsLoading(true);
+    Api.Officials.GetWithParameters({ 'forOperationalYear' : operationalYear }).then((data) => {
+      setOfficials(data)
+      setIsLoading(false);
     });
-  }, []);
+  }, [operationalYear]);
 
   //Used for setting titles for category
   const titles = [];
@@ -21,53 +33,40 @@ export default function OfficialsBoard() {
       : null
   );
 
-  //Used for mapping emails to correct category
-  let emails = [];
-  officials.map((official) => {
-    var email_obj = {};
-    console.log("official.post", official.post.category);
-    email_obj.category = `${official.post.category}`;
-    email_obj.email = `${
-      official.post.email
-        ? `${official.post.email}`
-        : "medieteknik@medieteknik.com"
-    }`;
-    emails.push(email_obj);
-  });
-
-  //an array of unique objects, used for mapping category with mail in render
-  let unique = [...new Set(emails.map((itm) => JSON.stringify(itm)))].map((i) =>
-    JSON.parse(i)
-  );
-
-  return (
-    <div id="officialsBoard">
-      <div className="officialsBoardHeader">Funktionärer</div>
-      {titles.map((title) => (
-        <div id="officialCategory">
-          <h3 className="committeeHeader">{title}</h3>
-          <h4 className="committeeMail" id="committeeMail">
-            {unique.map((item) => {
-              if (item.category === title) {
-                return `${item.email}`;
-              }
-            })}
-          </h4>
-
-          <div className="userListOfficial">
-            {officials
-              .filter((official) => official.post.category === title)
-              .map((official) => (
-                <UserCardOfficial
-                  key={official.user.id}
-                  user={official.user}
-                  subtitle={official.post.name}
-                  email={official.post.email}
-                />
-              ))}
-          </div>
+  return (<div className='officials-board-container'>
+    <div className='officials-board'>
+      <div className='officials-header'>
+        <h1><LocaleText phrase='common/officials'/></h1>
+        <div className='officials-select'>
+          <Dropdown 
+            options={operationalYears.map(year => { return {value: year, label: year}})}
+            onChange={(option) => setOperationalYear(option.value)}/>
         </div>
-      ))}
+      </div>
+
+      { isLoading ? <Spinner/> :
+         titles.map((title) => (
+          <div className='official-category' key={title}>
+            <div className='category-header'>
+              <h2>{title}</h2>
+              <p>medieteknik@medieteknik.com</p> {/*TODO: Add backend for post category email*/}
+            </div>
+
+            <div className='user-list-official'>
+              {officials
+                .filter((official) => official.post.category === title)
+                .map((official) => (
+                  <OfficialCard
+                    key={`${official.post.name}_${official.user.id}`}
+                    user={official.user}
+                    title={official.post.name}
+                    email={official.post.email}
+                  />
+                ))}
+            </div>
+          </div>
+        ))}
+    </div>
     </div>
   );
 }
