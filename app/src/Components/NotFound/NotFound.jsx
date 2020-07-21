b519b8250f258c6d11707266923b04c833c1544e
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2020-07-21 23:43:02 +0200

    Redigera sidor (och behörigheter)

--- app/src/Components/NotFound/NotFound.jsx
+++ app/src/Components/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ import { LocaleText } from '../../Contexts/LocaleContext';
 export default function NotFound() {
   return (
     <div>
-      <h1><LocaleText phrase="common/page_not_found" /></h1>
+      <h1><LocaleText phrase="page/page_not_found" /></h1>
     </div>
   );
 }
