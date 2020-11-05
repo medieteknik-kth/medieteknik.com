1864d909af86e76d996dda071d7046230be43108
Foppe <flundkvi@kth.se>
2020-11-05 12:03:39 +0100

    språk, formattering, stright boolin'

--- app/src/Components/Gallery/AlbumUpload.jsx
+++ app/src/Components/Gallery/AlbumUpload.jsx
@@ -1,24 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { LocaleText, localeDate } from '../../Contexts/LocaleContext'
+import { LocaleContext, LocaleText, translateToString } from '../../Contexts/LocaleContext'
 import Api from '../../Utility/Api'
 import './AlbumUpload.css'
 
 const AlbumUpload = () => {
     const [images, setImages] = useState([]);
     const [previewURLs, setPreviewURLs] = useState([])
-    const [title, setTitle] = useState("albumtitel")
+    const [title, setTitle] = useState(null)
+    const [includeDate, setIncludeDate] = useState(false)
+    const [description, setDescription] = useState(null)
+    const {lang} = useContext(LocaleContext);
+
     const handleFileChange = (files) => {
         console.log(files)
         const urls = Array.from(files).map(file => ({ url: URL.createObjectURL(file), title: file.name }))
         setImages(files)
         setPreviewURLs(urls)
     }
-
+    
     return (
         <div className="upload-container">
             <form encType="multipart/form-data" action="">
-                <h2><input className="title-input" type="text" placeholder={title} onChange={e => setTitle(e.target.value)}></input></h2>
+                <h2><input className="title-input" type="text" placeholder={translateToString({
+                    se: "Albumtitel",
+                    en: "Album name",
+                    lang
+                })} onChange={e => setTitle(e.target.value)}></input></h2>
+                <span className="desc-input" type="text" role="textbox" contentEditable placeholder={translateToString({
+                    se: "Beskrivning",
+                    en: "Description",
+                    lang
+                })}><LocaleText phrase="gallery/desc_placeholder"/></span>
                 {previewURLs && <div className="preview-container">
                     <div className="previews">
                         {
