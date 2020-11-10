7fb513d17f214e7bbb01ce4bc8ed944b7c858017
Foppe <flundkvi@kth.se>
2020-11-10 15:46:25 +0100

    närmare designen

--- app/src/Components/Gallery/AlbumUpload.jsx
+++ app/src/Components/Gallery/AlbumUpload.jsx
@@ -1,6 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useContext, useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { LocaleContext, LocaleText, translateToString } from '../../Contexts/LocaleContext'
+import { LocaleContext, LocaleText, translateToString } from '../../Contexts/LocaleContext';
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
+
 import Api from '../../Utility/Api'
 import './AlbumUpload.css'
 
@@ -9,43 +12,60 @@ const AlbumUpload = () => {
     const [previewURLs, setPreviewURLs] = useState([])
     const [title, setTitle] = useState(null)
     const [includeDate, setIncludeDate] = useState(false)
-    const [description, setDescription] = useState(null)
-    const {lang} = useContext(LocaleContext);
+    const [photographer, setPhotographer] = useState(null)
+    const { lang } = useContext(LocaleContext);
 
     const handleFileChange = (files) => {
         console.log(files)
         const urls = Array.from(files).map(file => ({ url: URL.createObjectURL(file), title: file.name }))
-        setImages(files)
-        setPreviewURLs(urls)
+        setImages([...images, ...files])
+        setPreviewURLs([...previewURLs, ...urls])
     }
-    
+
+    const removeImage = (imgToRemove, previewToRemove) => {
+        setImages(images.filter(img => !(img.name === imgToRemove)))
+        setPreviewURLs(previewURLs.filter(url => !(url === previewToRemove)))
+    }
+
     return (
         <div className="upload-container">
             <form encType="multipart/form-data" action="">
-                <h2><input className="title-input" type="text" placeholder={translateToString({
-                    se: "Albumtitel",
-                    en: "Album name",
-                    lang
-                })} onChange={e => setTitle(e.target.value)}></input></h2>
-                <span className="desc-input" type="text" role="textbox" contentEditable placeholder={translateToString({
-                    se: "Beskrivning",
-                    en: "Description",
-                    lang
-                })}><LocaleText phrase="gallery/desc_placeholder"/></span>
-                {previewURLs && <div className="preview-container">
-                    <div className="previews">
-                        {
-                            previewURLs.map(preview => (<div className="preview" key={preview.url}>
-                                <img className="preview-img" src={preview.url} />
-                                <p className="img-title">{preview.title}</p>
-                            </div>))
-                        }
+                <h2>Ladda upp bilder</h2>
+                <div className="info-container">
+                    <div>
+                        <label style={{ display: "none" }}>Namn på album</label>
+                        <input type="text" onChange={e => setTitle(e.target.value)} placeholder="Namn på album"></input>
                     </div>
-                </div>}
-                <label for="files" className="upload-button">{<LocaleText phrase="gallery/select_images" />}</label>
-                <input type="file" name="fileInput" accept="image/png, image/jpeg" onChange={e => handleFileChange(e.target.files)} multiple id="files" style={{ visibility: "hidden" }}></input>
+                    <div>
+                        <label style={{ display: "none" }}>Namn på fotograf</label>
+                        {/* Har fått för mig att detta är bra för screen readers */}
+                        <input type="text" onChange={e => setPhotographer(e.target.value)} placeholder="Namn på fotograf"></input>
+                    </div>
+                </div>
+                <div className="info-container">
+                    <div>
+                        <input type="date" placeholder="datum"></input>
+                    </div>
+                </div>
+                <div className="preview-container">
+                    {previewURLs &&
+                        <div className="previews">
+                            {
+                                previewURLs.map(preview => (<div className="preview" key={preview.url} >
+                                    
+                                    <img className="preview-img" src={preview.url}/>
+                                        <FontAwesomeIcon className="cancel-img-upload" onClick={(e) => { e.preventDefault(); removeImage(preview.title, preview) }} size="2x" icon={faTimes} color="#f0c900" />
+                                </div>))
+                            }
+                        </div>
+                    }
+                    <div className="yellow-image-upload">
+                        <label htmlFor="files" ><FontAwesomeIcon size="2x" icon={faPlus} color="#f0c900" /></label>
+                        <input type="file" name="files" accept="image/png, image/jpeg" onChange={e => handleFileChange(e.target.files)} multiple id="files" style={{ visibility: "hidden" }}></input>
+                    </div>
+                </div>
             </form>
-        </div>
+        </div >
     )
 }
 
