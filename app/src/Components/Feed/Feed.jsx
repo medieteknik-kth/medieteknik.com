3a69d53d8ad0268cc6ddff3cbacc077e1991541c
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2020-12-19 17:38:37 +0100

    Flyttade eventlistan till aktuellt

--- app/src/Components/Feed/Feed.jsx
+++ app/src/Components/Feed/Feed.jsx
@@ -4,11 +4,15 @@ import Api from '../../Utility/Api.js'
 import FeedCard, { feedTypes } from './FeedCard/FeedCard.jsx';
 import { LocaleText, translate } from '../../Contexts/LocaleContext';
 import Spinner from '../Common/Spinner/Spinner.jsx';
+import Button from '../Document/ViewDocuments/Assets/ButtonRasmus';
+import { useHistory } from 'react-router-dom';
 
 const Feed = (props) => {
 
     const [cont, setCont] = useState();
     const [isLoading, setIsLoading] = useState(true);
+
+    const history = useHistory();
 
     useEffect(() => {
         // Fetch events and posts
@@ -39,6 +43,7 @@ const Feed = (props) => {
 
     return (<div className='feed-container'>
         {props.landingTitle ? <h3 className='landing-title'><span><LocaleText phrase='feed/header'/></span></h3> : <h1><LocaleText phrase='feed/header'/></h1>}
+        <Button onClick={() => history.push("/eventList")}>Kommande event</Button>
         { isLoading ? <Spinner/> :
             <div className='feed-cards'>
                 { cont ? cont.map((post, i) =>
