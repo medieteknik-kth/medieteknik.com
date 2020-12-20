bf3e10d1960dd5e9662a4506dbf9d73f524a7b63
Rasmus Rudling <rasmusrudling@gmail.com>
2020-12-20 21:55:33 +0100

    Byter branch

--- app/src/Components/Gallery/Album/Album.jsx
+++ app/src/Components/Gallery/Album/Album.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './Album.css';
 import { useParams } from 'react-router-dom';
 import AlbumModal from '../AlbumModal/AlbumModal';
@@ -6,60 +6,93 @@ import Api from '../../../Utility/Api';
 
 
 const Album = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [currentImage, setCurrentImage] = useState(null);
-  const [data, setData] = useState(null);
-  const { id } = useParams();
+    const [modalOpen, setModalOpen] = useState(false);
+    const [currentImage, setCurrentImage] = useState(null);
+    const [currentImageId, setCurrentImageId] = useState(1);
+    const [album, setAlbum] = useState(null);
+    const { id } = useParams();
+    
 
-  useEffect(() => {
-    Api.Albums.GetById(id).then((album) => {
-      setData(album);
-    });
-  }, []);
+    const changeImage = useCallback(event => {
+        if (event.key === 'ArrowLeft') {
+            viewPreviousImage(currentImageId);
+        } else if (event.key === 'ArrowRight') {
+            viewNextImage(currentImageId);
+        }
+    }, [currentImageId])
 
-  const viewImage = (image) => {
-    setCurrentImage(image);
-    setModalOpen(true);
-  };
+    useEffect(() => {
+        Api.Albums.GetById(id).then((album) => {
+            setAlbum(album);
+        });
+        window.addEventListener('keydown', changeImage);
+        return () => window.removeEventListener('keydown', changeImage);
+    }, [currentImageId])
 
-  console.log(currentImage == null ? false : currentImage);
+    const viewPreviousImage = (imageId) => {
+        if (imageId > 0) {
+            setCurrentImageId(imageId - 1);
+            const tempImage = album.images[imageId - 1]
+            setCurrentImage({src: tempImage.url, title: tempImage.title, date: new Date(tempImage.date), photographer: tempImage.photographer})
+        }
+    }
 
-  return (
-    data == null ? <div /> : (
-      <>
-        { currentImage
-          ? (
-            <AlbumModal
-              image={currentImage.src}
-              title={currentImage.title}
-              date={new Date(currentImage.date)}
-              photographer={currentImage.photographer}
-              modalOpen={modalOpen}
-              setModalOpen={setModalOpen}
-            />
-          ) : <></>}
-        <div className="album-header">
-          <h2>{data.title}</h2>
-          <h5>{data.date ? new Date(data.date).toLocaleDateString('sv-SE', { day: 'numeric', month: 'short', year: 'numeric' }).replace(/ /g, ' ') : <></>}</h5>
-        </div>
-        <div className="album-container">
-          <div className="album-grid">
-            {data.images.map((image) => (
-              <div
-                key={image.url}
-                className="album-cell no-select"
-                onClick={() => viewImage({
-                  src: image.url, title: data.title, date: image.date, photographer: image.photographer,
-                })}
-              >
-                <img src={image.url} className="responsive-image" alt="" />
-              </div>
-            ))}
-          </div>
-        </div>
-      </>
-    )
-  );
+    const viewNextImage = (imageId) => {
+        if (imageId < album.images.length - 1) {
+            setCurrentImageId(imageId + 1);
+            const tempImage = album.images[imageId + 1]
+            setCurrentImage({src: tempImage.url, title: tempImage.title, date: new Date(tempImage.date), photographer: tempImage.photographer})
+        }
+    }
+
+    const viewImage = (imageId) => {
+        const tempImage = album.images[imageId];
+        setCurrentImage({src: tempImage.url, title: tempImage.title, date: new Date(tempImage.date), photographer: tempImage.photographer})
+        setModalOpen(true);
+    }
+
+    return (
+        album == null ? <div /> : (
+        <>
+            { currentImage
+            ? (
+                <AlbumModal
+                    image={currentImage.src}
+                    title={currentImage.title}
+                    date={new Date(currentImage.date)}
+                    photographer={currentImage.photographer}
+                    imageId={currentImageId}
+                    viewPreviousImage={viewPreviousImage}
+                    viewNextImage={viewNextImage}
+                    modalOpen={modalOpen}
+                    setModalOpen={setModalOpen}
+                />
+            ) : <></>}
+
+            <div className="album-header">
+            <h2>{album.title}</h2>
+            <h5>{album.date ? new Date(album.date).toLocaleDateString('sv-SE', { day: 'numeric', month: 'short', year: 'numeric' }).replace(/ /g, ' ') : <></>}</h5>
+            </div>
+            <div className="album-container">
+            <div className="album-grid">
+                {album.images.map((image, key) => (
+                    <div
+                        key={image.url}
+                        className="album-cell no-select"
+                        onClick={() => {
+                            console.log(key)
+                            setCurrentImageId(key);
+                            viewImage(key);
+                        }}
+                    >
+                    <img src={image.url} className="responsive-image" alt="" />
+                </div>
+                ))}
+            </div>
+            </div>
+        </>
+        )
+    );
 };
 
 export default Album;
