d3f5eae0287c510563b8ac848224fcb4a9a23e40
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2021-07-08 20:04:43 +0200

    Visa alla möjliga poster när man skapar funktionärer

--- app/src/Components/AdminTools/OfficialsAdmin.jsx
+++ app/src/Components/AdminTools/OfficialsAdmin.jsx
@@ -23,7 +23,7 @@ export const OfficialsFilter = (props) => (
 export function OfficialCreate(props) {
   return (<Create {...props}>
     <SimpleForm>
-      <ReferenceInput label="Post" source="post.id" reference="committee_posts" validate={[required()]}>
+      <ReferenceInput label="Post" source="post.id" reference="committee_posts" validate={[required()]} perPage={10000}>
         <SelectInput source="postId" optionText="name" />
       </ReferenceInput>
       <ReferenceInput label="Person" source="user.id" reference="users" validate={[required()]} perPage={10000}>
@@ -52,7 +52,7 @@ export function OfficialList(props) {
 export function OfficialEdit(props) {
   return (<Edit {...props}>
     <SimpleForm>
-      <ReferenceInput label="Post" source="post.id" reference="committee_posts" validate={[required()]}>
+      <ReferenceInput label="Post" source="post.id" reference="committee_posts" validate={[required()]} perPage={10000}>
         <SelectInput source="postId" optionText="name" />
       </ReferenceInput>
       <ReferenceInput label="Person" source="user.id" reference="users" validate={[required()]} perPage={10000}>
