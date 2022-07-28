cec988117669b2f8dd77bc0313139c1a140a5594
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2022-07-28 21:44:59 +0200

    Test for reception mode

--- api/api/resources/health.py
+++ api/api/resources/health.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from flask_restful import Resource
+from api.utility.receptionmode import RECEPTION_MODE
 
 class HealthResource(Resource):
     def get(self):
-        return jsonify("ok")
+        return jsonify("ok, reception_mode " + str(RECEPTION_MODE))
