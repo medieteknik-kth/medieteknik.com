8d4a865405904b813cb9990861bac0cc02b36222
Duplomar <martin.neihoff@outlook.com>
2022-11-14 11:20:17 +0100

    Add (very basic) calender

--- app/src/Components/Feed/Feed.jsx
+++ app/src/Components/Feed/Feed.jsx
@@ -66,6 +66,9 @@ const Feed = (props) => {
             </div>
         </div>
         }
+    <div style = {{display: 'flex', justifyContent: 'center'}}>
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FStockholm&title=Sektionen%20f%C3%B6r%20Medieteknik&src=Y182cm1jMHRvcDRyYzRkZDM1bW10Yjl2Z21uMEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23F6BF26" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </div>
     </div>);
 }
 
