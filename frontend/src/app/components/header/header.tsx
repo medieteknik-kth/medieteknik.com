c5b98465172cf1bfd6cf8a2a51bd3cd0b6cb0c87
André Eriksson <andretech1@hotmail.com>
2024-01-05 20:03:12 +0100

    Added more styling
    Added styling to the login button, also added the centre text

--- frontend/src/app/components/header/header.tsx
+++ frontend/src/app/components/header/header.tsx
@@ -82,8 +82,8 @@ export default function Header () {
           </a>
         </div>
 
-        <div className='w-1/2 h-full flex justify-end'>
-          <ul className='w-1/2 flex justify-between items-center text-sm tracking-wide'>
+        <div className='w-1/2 h-full flex justify-end items-center'>
+          <ul className='w-1/2 h-full flex justify-between items-center text-sm tracking-wide'>
             {HeaderNavElements.map((element, index) => {
               return (
                 <li key={index} className='w-full h-full'>
@@ -91,9 +91,13 @@ export default function Header () {
                 </li>
               )
             })}
+            
           </ul>
           <div className='w-1/4 h-full flex justify-end items-center mx-8'>
-            Logga In
+            <a href='/login' className='h-full flex items-center'>
+              <p className='mr-4 text-sm font-semibold tracking-wide'>&nbsp;&nbsp; LOGGA IN</p>
+              <svg className='w-10' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+            </a>
           </div>
         </div>
       </div>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,6 +11,12 @@ export default function Home() {
         <div className='w-full h-full absolute -z-10 top-0'>
           <Image src={Background.src} alt='bg' layout='fill' objectFit='cover' />
         </div>
+        <div className='w-full h-full z-10 flex justify-center'>
+          <div className='w-1/2 h-full flex flex-col items-center justify-center'>
+            <h1 className='text-7xl text-yellow-400 font-semibold tracking-wide p-8'>MEDIETEKNIK</h1>
+            <h2 className='text-2xl text-white tracking-widest'>KUNGLIGA TEKNISKA HÖGSKOLAN</h2>
+          </div>
+        </div>
       </div>
       <Footer />
     </main>
