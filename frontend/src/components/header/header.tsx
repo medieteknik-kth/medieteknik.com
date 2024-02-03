b869ee63e0768dda35083bce70c9bf07aefb62a8
Viggo Halvarsson Skoog <127208807+BoViKa@users.noreply.github.com>
2024-02-03 22:16:16 +0100

    Header fix
    added the needed nav bar buttons and added hover css

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -17,23 +17,23 @@ export default async function Header ({ params: { language } }: { params: { lang
   const headerElements: HeaderElement[] = t('navs', { returnObjects: true });
 
   return (
-    <header className='w-full h-24 text-white fixed bg-white/25'>
+    <header className='w-full h-24 text-white fixed bg-transparent backdrop-blur'>
       <div className='w-full h-full flex justify-between'>
-        <div className='w-1/2 h-full flex justify-between md:justify-start items-center'>
+        <div className='w-full h-full flex justify-between md:justify-start items-center'>
           <Link href='/' 
           className='w-20 h-full px-4 flex justify-center sm:justify-start items-center' 
           aria-label='Home Button' role='button' title='Home'>
-            <Image src={Logo.src} alt='Medieteknik Logo' width='46' height='46' /> 
+            <Image src={Logo.src} alt='Medieteknik Logo' width='46' height='46' />
           </Link>
 
-          <div className='w-10/12 2xl:w-1/2 h-full ml-16'>
+          <div className='w-full 2xl:w-1/2 h-full ml-16'>
             <ul className='w-full h-full flex justify-between uppercase tracking-widest text-sm'>
               {headerElements.map((element: HeaderElement, index: number) => {
                 return (
                   <li key={index} className='w-28 h-full'>
                     <Link 
                     href={element.link} 
-                    className='w-full h-full grid place-items-center hover:bg-black/25' 
+                    className='w-full h-full grid place-items-center border-b-2 border-transparent hover:border-yellow-400' 
                     aria-label={element.title} role='button' 
                     title={element.title}>
                       {element.title}
