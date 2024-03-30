c2b5eaf2ea0801c429f68ef0ac4f50bdc35fa1ee
André Eriksson <andretech1@hotmail.com>
2024-03-30 18:48:46 +0100

    Changed 'SV' to 'SE' for accuracy
    Changed it to reflect the language code of Sweden which is SE. Also updated Next.JS to 14.1.4

--- frontend/src/app/i18n/settings.tsx
+++ frontend/src/app/i18n/settings.tsx
@@ -1,17 +1,19 @@
-import {InitOptions} from 'i18next';
-export const fallbackLanguage: string = 'en';
-export let supportedLanguages: string[] = [fallbackLanguage, 'sv'];
-export const cookieName: string = 'language';
-export const defaultNS: string = 'translation';
+import { InitOptions } from 'i18next'
+export const fallbackLanguage: string = 'en'
+export let supportedLanguages: string[] = [fallbackLanguage, 'se']
+export const cookieName: string = 'language'
+export const defaultNS: string = 'translation'
 
-
-export function getOptions(lng:string = fallbackLanguage, ns:string = defaultNS): InitOptions {
+export function getOptions(
+  lng: string = fallbackLanguage,
+  ns: string = defaultNS
+): InitOptions {
   return {
-      supportedLngs: supportedLanguages,
-      fallbackLng: fallbackLanguage,
-      lng,
-      fallbackNS: defaultNS,
-      defaultNS,
-      ns
-  };
+    supportedLngs: supportedLanguages,
+    fallbackLng: fallbackLanguage,
+    lng,
+    fallbackNS: defaultNS,
+    defaultNS,
+    ns,
+  }
 }
