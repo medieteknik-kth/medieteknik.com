702784af343d17eae45b78896c2ba4eb4f90d91d
André Eriksson <andretech1@hotmail.com>
2024-04-15 18:41:33 +0200

    Updated Versions
    And updated header element (news)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,23 +4,28 @@ module.exports = {
     webpack(config) {
         
         const fileLoaderRule = config.module.rules.find((rule) =>
-        rule.test?.test?.('.svg'),
+            rule.test?.test?.('.svg')
         );
 
-        config.module.rules.push({
-            ...fileLoaderRule,
-            test: /\.svg$/i,
-            resourceQuery: /url/,
-        },
-        {
-            test: /\.svg$/i,
-            issuer: fileLoaderRule.issuer,
-            resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/]},
-            use: ['@svgr/webpack'],
-        },
-        )
-        fileLoaderRule.exclude = /\.svg$/i
+        if (fileLoaderRule) {
+            // Exclude SVGs from the existing rule
+            fileLoaderRule.exclude = /\.svg$/i;
 
+            // Add new rule for handling SVGs with `@svgr/webpack`
+            config.module.rules.push({
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [/url/] },
+                use: ['@svgr/webpack']
+            });
+
+            // Add or adjust rule for using SVGs as URLs
+            config.module.rules.push({
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/,
+            });
+        }
         return config;
     }
-}
+};
