0d93d03779a3dfbc80adb3e0f7be0a5fcd4d6094
André Eriksson <andretech1@hotmail.com>
2024-05-17 07:45:58 +0200

    [backend] Added CSRF, CORS, Rate Limits
    Added protection and safety features, OAuth is coming later

--- backend/config.py
+++ backend/config.py
@@ -2,3 +2,9 @@ import os
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_ECHO = True
+
+KTH_CLIENT_ID = os.environ.get('KTH_CLIENT_ID')
+KTH_CLIENT_SECRET = os.environ.get('KTH_CLIENT_SECRET')
+KTH_ACCESS_TOKEN_URL = 'https://login.ug.kth.se/adfs/oauth2/token'
+KTH_AUTHORIZE_URL = 'https://login.ug.kth.se/adfs/oauth2/authorize'
+KTH_API_BASE_URL = 'https://app.kth.se/api/v.1.1'
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_wtf.csrf import CSRFProtect
+# from decorators.authorization import oauth
 from utility.database import db
 import os
 
@@ -6,7 +11,34 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'secret')
 app.config.from_object('config')
 
+# Enable CORS
+CORS(app, supports_credentials=True, 
+        origins=['http://localhost:3000', 'https://medieteknik.com'], allow_headers=['Content-Type', 'Authorization'], 
+        expose_headers=['Content-Type', 'Authorization'], 
+        methods=['GET', 'POST', 'PUT', 'DELETE'],
+        max_age=86400
+     )
+
 db.init_app(app)
+
+# CSRF protection 
+csrf = CSRFProtect(app)
+
+# Rate limiting for API
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["100 per day", "20 per hour"],
+    storage_uri=os.environ.get('REDIS_URL', 'memory://'),
+)
+
+# OAuth
+# oauth.init_app(app)
+# oauth.register('kth', kwargs={
+#     'scope': 'openid email',
+#     'access type': 'offline',
+#     'response_type': 'token',
+# })
 
 if __name__ == '__main__':
     app.run(debug=True)
