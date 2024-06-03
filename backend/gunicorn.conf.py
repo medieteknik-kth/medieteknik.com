88ed9d9e33904e20c0bbc735aebbd96b4899a1e7
André Eriksson <andretech1@hotmail.com>
2024-06-03 11:45:01 +0200

    [backend] Updated Python and gunicorn Config

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,10 +1,10 @@
 import multiprocessing
 import os
 from dotenv import load_dotenv
-loglevel = "info"
+loglevel = "debug"
 workers = 3 * round(multiprocessing.cpu_count() / 2) + 1
 bind = "0.0.0.0:8000"
-
+timeout = 120
 
 env = os.path.join(os.getcwd(), '.env')
 if os.path.exists(env):
