c5b6c75966ee5a327f1430f2fac38ca700a27032
André Eriksson <andretech1@hotmail.com>
2024-07-14 15:27:36 +0200

    [frontend] UI Adjustments

--- frontend/src/app/[language]/about.tsx
+++ frontend/src/app/[language]/about.tsx
@@ -43,29 +43,29 @@ export default async function About({ language }: { language: string }) {
   ]
 
   return (
-    <section className='w-full h-fit desktop:h-[640px] relative bg-white dark:bg-[#111] px-4 lg:px-20 2xl:px-72 py-10 border-t-2 flex flex-col justify-around border-black/75 dark:border-white/75'>
+    <section className='w-full h-fit desktop:h-[640px] relative bg-white dark:bg-[#111] px-4 lg:px-20 2xl:px-56 py-10 border-t-2 flex flex-col justify-around border-black/75 dark:border-white/75'>
       <Image
         src={Logo.src}
         alt='logo'
-        width={320}
-        height={320}
+        width={280}
+        height={280}
         loading='lazy'
         placeholder='empty'
         className='absolute left-8 top-0 bottom-0 my-auto opacity-35 hidden md:block'
       />
       <div className='flex flex-col lg:flex-row justify-between items-center mb-10 desktop:mb-0'>
-        <h2 className='text-2xl xs:text-5xl font-bold w-full lg:w-80 py-2 lg:py-0 tracking-wider text-center lg:text-start'>
+        <h2 className='text-2xl xs:text-5xl font-bold w-full lg:w-fit py-2 lg:py-0 tracking-wider text-center lg:text-start'>
           {t('about')}
         </h2>
         <div className='w-full mb-2 lg:mb-0 py-2 xl:py-0 lg:w-[450px] desktop:w-[750px] justify-self-center text-md md:text-lg'>
           {t('description')}
         </div>
       </div>
-      <div className='flex flex-wrap gap-2 flex-col desktop:flex-row justify-around items-center'>
+      <div className='flex flex-wrap gap-2 flex-col desktop:flex-row justify-around items-center px-4'>
         {cards.map((card) => (
           <div
             key={card.title}
-            className='card w-full lg:w-[720px] desktop:w-[400px] h-64 bg-slate-100 px-8 py-4 rounded-xl flex flex-col justify-between items-center relative'
+            className='card w-full lg:w-[720px] desktop:w-[450px] h-64 bg-slate-100 px-8 py-4 rounded-xl flex flex-col justify-between items-center relative'
           >
             <Link
               href={card.href}
