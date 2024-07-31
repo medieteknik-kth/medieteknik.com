966dfa56adde8c17473f8adb0d53afd3317c3899
André Eriksson <andretech1@hotmail.com>
2024-07-31 12:26:31 +0200

    [frontend] New API calls

---
+++ frontend/src/api/student.ts
@@ -0,0 +1,39 @@
+import { cache } from 'react';
+import api from './index';
+import Student, { IndividualCommitteePosition, Profile } from '@/models/Student';
+import { EventPagniation, NewsPagination } from '@/models/Pagination';
+
+export const GetStudentPublic = cache(async (student: string, language_code: string, detailed: boolean = false) => {
+  const response = await api.get(`/public/students/${student}?language=${language_code}&detailed=${detailed}`);
+
+  if (response.status === 200) {
+      return response.data as {
+        student: Student,
+        profile?: Profile,
+        memberships: IndividualCommitteePosition[]
+      }
+  }
+
+  return null
+})
+
+
+export const GetStudentNews = cache(async (student_email: string, language_code: string) => {
+  const response = await api.get(`/public/news/student/${student_email}?language=${language_code}`);
+
+  if (response.status === 200) {
+      return response.data as NewsPagination
+  }
+
+  return null
+})
+
+export const GetStudentEvents = cache(async (student_email: string, language_code: string) => {
+  const response = await api.get(`/public/events/student/${student_email}?language=${language_code}`);
+
+  if (response.status === 200) {
+      return response.data as EventPagniation
+  }
+
+  return null
+})
