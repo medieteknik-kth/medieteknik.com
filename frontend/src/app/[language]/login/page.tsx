c356868b99360e7ada4426db05ed74ca77e9d416
André Eriksson <andreeriksson444@gmail.com>
2024-10-14 16:31:57 +0200

    [frontend] Improved Login Page
    * Better responsiveness
    * Translations for both Swedish and English
    * More documentation
    * HTML Title

--- frontend/src/app/[language]/login/page.tsx
+++ frontend/src/app/[language]/login/page.tsx
@@ -1,3 +1,22 @@
+import { useTranslation } from '@/app/i18n'
+import { Metadata, ResolvingMetadata } from 'next'
 import Login from './login'
+
+interface Params {
+  language: string
+}
+
+export async function generateMetadata(
+  { params }: { params: Params },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { t } = await useTranslation(params.language, 'login')
+  const value = t('login')
+
+  const capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1)
+  return {
+    title: capitalizedValue,
+  }
+}
 
 export default Login
