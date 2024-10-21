3e2fa517c74809deffe8f9ea4890bb2b626f0402
André Eriksson <andreeriksson444@gmail.com>
2024-10-22 01:08:15 +0200

    [frontend] Disabled the explore more section
    * Adjustments to the explore more card; it is now a media card instead of recruitment

--- frontend/src/app/[language]/chapter/committees/[committee]/client/explore.tsx
+++ frontend/src/app/[language]/chapter/committees/[committee]/client/explore.tsx
@@ -1,10 +1,6 @@
 'use client'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import {
-  DocumentDuplicateIcon,
-  HashtagIcon,
-  UsersIcon,
-} from '@heroicons/react/24/outline'
+import { UsersIcon } from '@heroicons/react/24/outline'
 
 export default function ExploreMore({
   language,
@@ -23,7 +19,7 @@ export default function ExploreMore({
           <Card
             className='bg-yellow-100 dark:bg-yellow-300 hover:scale-110 transition-transform cursor-pointer dark:text-black'
             onClick={() => {
-              location.href = '/recruitment?committee=' + committee
+              location.href = '/chapter/media/' + committee
             }}
           >
             <CardHeader>
@@ -32,49 +28,11 @@ export default function ExploreMore({
               </div>
             </CardHeader>
             <CardContent>
-              <CardTitle>Recruitment</CardTitle>
+              <CardTitle>Media</CardTitle>
               <p className='max-w-xs'>
-                Want to be part of{' '}
+                See the latest media from{' '}
                 <span className='capitalize font-bold'>{committeeName}</span>?
-                Click here to see active recruitment.
-              </p>
-            </CardContent>
-          </Card>
-          <Card
-            className='bg-red-100 dark:bg-red-300 hover:scale-110 transition-transform cursor-pointer dark:text-black'
-            onClick={() => {
-              location.href = '/bulletin?committee=' + committee
-            }}
-          >
-            <CardHeader>
-              <div className='w-16 h-16 grid place-items-center border rounded-full bg-red-400 dark:bg-red-500'>
-                <DocumentDuplicateIcon className='w-6 h-6 text-white' />
-              </div>
-            </CardHeader>
-            <CardContent>
-              <CardTitle>News and Events</CardTitle>
-              <p className='max-w-xs'>
-                See the latest news and events from{' '}
-                <span className='capitalize font-bold'>{committeeName}</span>.
-              </p>
-            </CardContent>
-          </Card>
-          <Card
-            className='bg-sky-100 dark:bg-sky-300 hover:scale-110 transition-transform cursor-pointer dark:text-black'
-            onClick={() => {
-              location.href = '/bulletin?committee=' + committee
-            }}
-          >
-            <CardHeader>
-              <div className='w-16 h-16 grid place-items-center border rounded-full bg-sky-400 dark:bg-sky-500'>
-                <HashtagIcon className='w-6 h-6 text-white' />
-              </div>
-            </CardHeader>
-            <CardContent>
-              <CardTitle>Social Media</CardTitle>
-              <p className='max-w-xs'>
-                Follow us on social media and see what happens in{' '}
-                <span className='capitalize font-bold'>{committeeName}</span>.
+                Click here to see latest media
               </p>
             </CardContent>
           </Card>
