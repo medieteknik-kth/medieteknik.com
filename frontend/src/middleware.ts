ecf10db28c8526b41c58c982c26ad4d0509b87e5
André Eriksson <andreeriksson444@gmail.com>
2025-03-14 10:17:37 +0100

    [frontend] Major change to middleware
    Now is less bloated and will hopefully reduce the wait time between pages, and less issues with handling languages.

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -27,7 +27,9 @@ const BLACKLISTED_URLS_REGEX = new RegExp(
     '^/manifest\\.webmanifest',
     '^/favicon',
     '^/icon',
+    '^/service-worker\\.js',
     '^/web-app-manifest-192x192\\.png',
+    '^/web-app-manifest-192x192-maskable\\.png',
     '^/web-app-manifest-512x512\\.png',
     '^/screenshots',
     '^/apple-icon\\.png',
@@ -41,57 +43,135 @@ const BLACKLISTED_URLS_REGEX = new RegExp(
 /**
  * Handles the language redirection and cookie setting based on the request headers and cookies.
  *
- * @param {NextRequest} request - The request object containing headers.
- * @return {NextResponse} The response object with redirection or next response.
+ * @param {NextRequest} request - The incoming request object.
+ * @return {Promise<NextResponse>} - The response object with the appropriate redirection or cookie setting.
  */
-async function handleLanguage(
-  request: NextRequest
-): Promise<NextResponse | string | undefined> {
+async function handleLanguage(request: NextRequest): Promise<NextResponse> {
   // Blacklisted URLs, which should not be redirected
   const isBlacklisted = BLACKLISTED_URLS_REGEX.test(request.nextUrl.pathname)
+  let response: NextResponse = NextResponse.next()
 
   if (isBlacklisted) {
+    return response
+  }
+
+  const isLanguageCookiePresent = request.cookies.has(LANGUAGE_COOKIE_NAME)
+
+  /**
+   * @name getPathLanguage
+   * @description Gets the language from the path of the URL, e.g. /en or /sv.
+   *
+   * @returns {LanguageCode | undefined} - The language code if found, otherwise undefined.
+   */
+  function getPathLanguage(): LanguageCode | undefined {
+    const path = request.nextUrl.pathname
+    const [_, language] = path.split('/')
+    if (SUPPORTED_LANGUAGES.includes(language as LanguageCode)) {
+      return language as LanguageCode
+    }
     return undefined
   }
 
-  let response: NextResponse = new NextResponse()
+  let language = getPathLanguage()
 
+  if (language && !isLanguageCookiePresent) {
+    // If the user has navigated to a page with a language in the path and has never visited the site before.
+    response.cookies.set(LANGUAGE_COOKIE_NAME, language as string, {
+      path: '/',
+      sameSite: 'lax',
+      expires: new Date(Date.now() + 31_536_000_000),
+      secure: !isDevelopment,
+    })
+    return response
+  }
+
+  /**
+   * @name getCookieLanguage
+   * @description Gets the language from the cookie, if it exists.
+   *
+   * @returns {LanguageCode | undefined} - The language code if found, otherwise undefined.
+   */
   function getCookieLanguage(): LanguageCode | undefined {
     const languageCookie = request.cookies.get('language')
     if (languageCookie) {
+      console.log('Found language in cookie:', languageCookie.value)
       return languageCookie.value as LanguageCode
     }
     return undefined
   }
 
+  /**
+   * @name getBrowserLanguage
+   * @description Gets the language from the browser's Accept-Language header.
+   *
+   * @returns {LanguageCode | undefined} - The language code if found, otherwise undefined.
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
+   */
   function getBrowserLanguage(): LanguageCode | undefined {
     const acceptLanguage = request.headers.get('Accept-Language')
     if (acceptLanguage) {
-      const [languageCode] = acceptLanguage.split(',')[0].split('-')
-      if (SUPPORTED_LANGUAGES.includes(languageCode as LanguageCode)) {
-        return languageCode as LanguageCode
+      const languages = acceptLanguage
+        .split(',')
+        .map((lang) => {
+          const [code, quality] = lang.split(';q=')
+          return {
+            code: code.split('-')[0].trim() as LanguageCode,
+            quality: quality ? Number.parseFloat(quality) : 1,
+          }
+        })
+        .sort((a, b) => b.quality - a.quality)
+
+      for (const { code } of languages) {
+        if (SUPPORTED_LANGUAGES.includes(code)) {
+          console.log('Found language in browser:', code)
+          return code as LanguageCode
+        }
       }
     }
     return undefined
   }
 
-  function getRefererLanguage(): LanguageCode | undefined {
+  /**
+   * @name getReferrerLanguage
+   * @description Gets the language from the Referer header, if it exists.
+   *
+   * @returns {LanguageCode | undefined} - The language code if found, otherwise undefined.
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referer
+   * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Origin
+   */
+  function getReferrerLanguage(): LanguageCode | undefined {
     const referer = request.headers.get('Referer')
     if (referer) {
       const url = new NextURL(referer)
       const language = SUPPORTED_LANGUAGES.find((locale) =>
         url.pathname.startsWith(`/${locale}`)
       )
+      console.log('Found language in referer:', language)
       return language || undefined
     }
     return undefined
   }
 
-  const language =
+  language =
     getCookieLanguage() ?? // Cookie language, taken from when a user has visited the site before and has a cookie set.
     getBrowserLanguage() ?? // Browser language, taken from when a user navigates to the site from a search engine or similar.
-    getRefererLanguage() ?? // Referer language, taken from when a user navigates to the site from another page.
+    getReferrerLanguage() ?? // Referer language, taken from when a user navigates to the site from another page.
     FALLBACK_LANGUAGE // Fallback language, used when no other language is found.
+
+  if (language === getPathLanguage()) {
+    // If the language in the path matches the detected language, do nothing.
+    return response
+  }
+
+  if (isLanguageCookiePresent) {
+    // If the cookie is already set, and the path is not the same as the detected language, replace the cookie.
+    response.cookies.set(LANGUAGE_COOKIE_NAME, language, {
+      path: '/',
+      sameSite: 'lax',
+      expires: new Date(Date.now() + 31_536_000_000),
+      secure: !isDevelopment,
+    })
+  }
 
   // Non-specified language or language not supported
   if (
@@ -101,37 +181,39 @@ async function handleLanguage(
   ) {
     if (isDevelopment) {
       console.warn(
-        `Redirecting to /${language}. Original path: ${request.nextUrl.pathname}`
+        `Redirecting to /${language}/${request.nextUrl.pathname.replace(/(.*)\/(?!.*\/)/, '') || ''}. Original path: ${request.nextUrl.pathname}`
       )
     }
 
     response = NextResponse.redirect(
       new NextURL(
-        `/${language}${request.nextUrl.pathname.replace(/^\/[^/]+/, '') || '/'}`,
+        `/${language}/${request.nextUrl.pathname.replace(/(.*)\/(?!.*\/)/, '') || ''}`,
         request.nextUrl
       )
     )
 
-    response.cookies.set(LANGUAGE_COOKIE_NAME, language, {
-      path: '/',
-      sameSite: 'lax',
-      expires: new Date(Date.now() + 31_536_000_000),
-      secure: !isDevelopment,
-    })
     return response
   }
 
-  return language
+  return response
 }
 
+/**
+ * @name generateCSP
+ * @description Generates a Content Security Policy (CSP) string based on the provided nonce and environment.
+ *
+ * @param {string} nonce - The nonce to be used in the CSP.
+ * @returns {string} - The generated CSP string.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+ */
 function generateCSP(nonce: string): string {
-  return `default-src 'none'; 
-    script-src 'self' https://vercel.live 'nonce-${nonce}';
-    connect-src 'self' ${process.env.NODE_ENV === 'development' ? 'localhost:80 http://localhost:*' : 'https://api.medieteknik.com'} https://vercel.live wss://ws-us3.pusher.com blob:; 
+  return `default-src 'self'; 
+    script-src 'self' 'nonce-${nonce}' ${process.env.NODE_ENV === 'development' ? "'unsafe-eval'" : ''} 'unsafe-inline';
+    connect-src 'self' ${process.env.NODE_ENV === 'development' ? 'http://localhost:80 http://localhost:* ws://localhost:3000' : 'https://api.medieteknik.com'} https://vercel.live https://www.kth.se wss://ws-us3.pusher.com blob: https://storage.googleapis.com https://va.vercel-scripts.com;
     media-src blob:; 
     img-src 'self' https://storage.googleapis.com https://vercel.live https://vercel.com https://i.ytimg.com data: blob:; 
     style-src 'self' https://vercel.live 'unsafe-inline'; 
-    font-src 'self' https://vercel.live https://assets.vercel.com; 
+    font-src 'self' https://vercel.live https://assets.vercel.com;
     frame-src 'self' https://www.youtube.com/ https://www.instagram.com https://vercel.live; 
     object-src 'none'; 
     base-uri 'none'; 
@@ -146,39 +228,17 @@ function generateCSP(nonce: string): string {
 }
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
-  const languageResponse = await handleLanguage(request)
+  const response = await handleLanguage(request)
   const nonce = crypto.randomUUID()
-  const csp = generateCSP(nonce)
-  const headers = new Headers(request.headers)
-  headers.set('Content-Security-Policy', csp)
-  headers.set('x-nonce', nonce)
+
+  response.headers.set('Content-Security-Policy', generateCSP(nonce))
+  response.headers.set('x-nonce', nonce)
   if (process.env.NODE_ENV === 'production') {
-    headers.set(
+    // TODO: Roll out longer HSTS in the future, ca 1 week to 604800, then 3 months to 63072000
+    response.headers.set(
       'Strict-Transport-Security',
-      'max-age=31536000; includeSubDomains; preload'
+      'max-age=3600; includeSubDomains'
     )
-  }
-
-  const response = NextResponse.next({ headers })
-
-  if (languageResponse instanceof NextResponse) {
-    languageResponse.headers.set('Content-Security-Policy', csp)
-    languageResponse.headers.set('x-nonce', nonce)
-    if (process.env.NODE_ENV === 'production') {
-      languageResponse.headers.set(
-        'Strict-Transport-Security',
-        'max-age=31536000; includeSubDomains; preload'
-      )
-    }
-    return languageResponse
-  }
-
-  if (typeof languageResponse === 'string') {
-    response.cookies.set(LANGUAGE_COOKIE_NAME, languageResponse, {
-      path: '/',
-      sameSite: 'lax',
-      expires: new Date(Date.now() + 31_536_000_000),
-    })
   }
 
   return response
