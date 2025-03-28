cbe9da73fef3514af6efc950517ef4881072c57f
André Eriksson <andreeriksson444@gmail.com>
2025-03-28 12:12:38 +0100

    Added schemas for the different templates

--- apps/frontend/rgbank/src/app/schemas/invoice.ts
+++ apps/frontend/rgbank/src/app/schemas/invoice.ts
@@ -1,17 +1,23 @@
 import { z } from 'zod'
 
-export const expenseSchema = z.object({
+export const invoiceSchema = z.object({
+  hasChapterPaid: z.boolean().optional(),
   files: z.array(z.instanceof(File)).refine((files) => files.length > 0, {
     message: 'At least one file is required',
   }),
+  description: z.string().min(1, { message: 'Description is required' }),
+  isOriginal: z.boolean().optional(),
+  isBooked: z.boolean().optional(),
   date: z.coerce.date().refine((date) => date <= new Date(), {
     message: 'Date must be in the past',
   }),
-  digital: z.boolean(),
+  dueDate: z.coerce.date().refine((date) => date > new Date(), {
+    message: 'Due date must be in the future',
+  }),
   categories: z
     .array(
       z.object({
-        id: z.number().optional().or(z.literal('')),
+        id: z.string().optional().or(z.literal('')),
         author: z.string().min(1, { message: 'Author is required' }),
         category: z.string().min(1, { message: 'Category is required' }),
         type: z.string().min(1, { message: 'Type is required' }),
