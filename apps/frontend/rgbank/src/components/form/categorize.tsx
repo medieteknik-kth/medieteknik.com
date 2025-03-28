954fd373f9c82b2124a3e5fed0c4c775ddc8dcf1
André Eriksson <andreeriksson444@gmail.com>
2025-03-28 12:12:54 +0100

    New components to use in the templates

---
+++ apps/frontend/rgbank/src/components/form/categorize.tsx
@@ -0,0 +1,289 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import type Committee from '@/models/Committee'
+import type { Category } from '@/models/Form'
+import { ChevronDownIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
+import Logo from 'public/images/logo.webp'
+import { useCallback, useEffect, useState } from 'react'
+
+interface Props {
+  defaultValue?: Category[]
+  setFormCategories: (categories: Category[]) => void
+  categoryStep: number
+  uncompleteStep: (step: number) => void
+  completeStep: (step: number) => void
+  committees: Committee[]
+}
+
+export default function Categorize({
+  defaultValue,
+  setFormCategories,
+  categoryStep,
+  uncompleteStep,
+  completeStep,
+  committees,
+}: Props) {
+  let categoryIndex = 0 // Used to generate unique IDs for categories
+  const [dropdownOpen, setDropdownOpen] = useState(-1) // -1 = none, i = index of the dropdown open
+  const [categories, setCategories] = useState<Category[]>([
+    ...(defaultValue && defaultValue.length > 0
+      ? defaultValue
+      : [
+          {
+            id: 0,
+            author: '',
+            category: '',
+            type: '',
+            amount: '0',
+          },
+        ]),
+  ])
+
+  const allCommittees = [
+    ...committees.map((committee) => ({
+      value: committee.committee_id,
+      label: committee.translations[0].title,
+      icon: committee.logo_url,
+    })),
+  ]
+
+  const addCategory = useCallback(() => {
+    categoryIndex++
+    setCategories((prev) => [
+      ...prev,
+      {
+        id: categoryIndex,
+        author: '',
+        category: '',
+        type: '',
+        amount: '0',
+      },
+    ])
+  }, [categoryIndex])
+
+  const validateCategories = useCallback(() => {
+    const isValid = categories.every(
+      (category) =>
+        category.author !== '' &&
+        category.category !== '' &&
+        category.type !== '' &&
+        category.amount !== '' &&
+        category.amount !== '0' &&
+        (Number.isNaN(category.amount)
+          ? false
+          : Number.parseFloat(category.amount) > 0)
+    )
+
+    if (isValid) {
+      completeStep(categoryStep)
+    } else {
+      uncompleteStep(categoryStep)
+    }
+  }, [categories, categoryStep, completeStep, uncompleteStep])
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: Update categories and validateCategories
+  useEffect(() => {
+    validateCategories()
+  }, [categories, validateCategories])
+
+  useEffect(() => {
+    setFormCategories(categories)
+  }, [categories, setFormCategories])
+
+  return (
+    <>
+      <ul className='space-y-2 mt-4'>
+        {categories.map((category, index) => (
+          <li key={category.id} className='grid grid-cols-12 gap-4'>
+            <div className='w-full flex flex-col gap-2 col-span-3'>
+              {index === 0 && (
+                <Label>
+                  Author <span className='text-red-500'>*</span>
+                </Label>
+              )}
+              <Popover
+                open={dropdownOpen === index}
+                onOpenChange={() => {
+                  setDropdownOpen((prev) => (prev === index ? -1 : index))
+                }}
+              >
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={'outline'}
+                    // biome-ignore lint/a11y/useSemanticElements: This is a shadcn/ui component for a combobox
+                    role='combobox'
+                    className='w-full items-center justify-between'
+                  >
+                    <div className='flex items-center gap-2'>
+                      <div className='bg-white p-1 rounded-lg'>
+                        <Image
+                          src={
+                            allCommittees.find(
+                              (committee) => committee.value === category.author
+                            )?.icon || Logo.src
+                          }
+                          alt='Committee'
+                          unoptimized
+                          width={24}
+                          height={24}
+                        />
+                      </div>
+                      {allCommittees.find(
+                        (committee) => committee.value === category.author
+                      )?.label || 'Select a committee'}
+                    </div>
+                    <ChevronDownIcon className='w-5 h-5' />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className='w-96! p-0'>
+                  <Command>
+                    <CommandInput placeholder='Search' />
+                    <CommandList>
+                      <CommandEmpty>'None found</CommandEmpty>
+                      <CommandGroup>
+                        {allCommittees.map((committee) => (
+                          <CommandItem
+                            key={committee.value}
+                            value={committee.value}
+                            onSelect={(currentValue) => {
+                              const newCategories = [...categories]
+                              newCategories[index].author = currentValue
+                              setDropdownOpen(-1)
+                              validateCategories()
+                            }}
+                            className='flex items-center justify-between'
+                          >
+                            <div className='flex items-center gap-2'>
+                              <div className='bg-white p-1 rounded-lg'>
+                                <Image
+                                  src={committee.icon}
+                                  alt={committee.label}
+                                  unoptimized
+                                  width={24}
+                                  height={24}
+                                />
+                              </div>
+                              {committee.label}
+                            </div>
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+            </div>
+
+            <div className='w-full flex flex-col gap-2 col-span-3'>
+              {index === 0 && (
+                <Label>
+                  Category <span className='text-red-500'>*</span>
+                </Label>
+              )}
+              <Input
+                type='text'
+                placeholder='Category'
+                value={category.category}
+                onBlur={(e) => {}}
+                onChange={(e) => {
+                  const newCategories = [...categories]
+                  newCategories[index].category = e.target.value
+                  setCategories(newCategories)
+                }}
+              />
+            </div>
+
+            <div className='w-full flex flex-col gap-2 col-span-3'>
+              {index === 0 && (
+                <Label>
+                  Type <span className='text-red-500'>*</span>
+                </Label>
+              )}
+
+              <Input
+                type='text'
+                placeholder='Type'
+                value={category.type}
+                onChange={(e) => {
+                  const newCategories = [...categories]
+                  newCategories[index].type = e.target.value
+                  setCategories(newCategories)
+                }}
+              />
+            </div>
+
+            <div className='w-full flex flex-col gap-2 col-span-2'>
+              {index === 0 && (
+                <Label>
+                  Amount (SEK) <span className='text-red-500'>*</span>
+                </Label>
+              )}
+              <Input
+                type='text'
+                placeholder='Amount'
+                pattern='[0-9]*([.,][0-9]*)?'
+                inputMode='numeric'
+                value={category.amount}
+                onChange={(e) => {
+                  const value = e.target.value
+                  if (/^[0-9]*([.,][0-9]*)?$/.test(value)) {
+                    // Only allow one comma or dot
+                    if (
+                      (value.match(/[,]/g) || []).length > 1 ||
+                      (value.match(/[.]/g) || []).length > 1
+                    ) {
+                      return
+                    }
+
+                    const newCategories = [...categories]
+                    newCategories[index].amount = value
+                    setCategories(newCategories)
+                  }
+                }}
+              />
+            </div>
+
+            <Button
+              className='col-span-1 mt-auto'
+              variant={'destructive'}
+              size={'icon'}
+              disabled={categories.length === 1 && index === 0}
+              onClick={() => {
+                const newCategories = [...categories]
+                newCategories.splice(index, 1)
+                setCategories(newCategories)
+              }}
+            >
+              <XMarkIcon className='w-4 h-4' />
+            </Button>
+          </li>
+        ))}
+      </ul>
+      <Button
+        className='mt-4'
+        variant={'outline'}
+        size={'sm'}
+        onClick={addCategory}
+      >
+        Add Category
+      </Button>
+    </>
+  )
+}
---
+++ apps/frontend/rgbank/src/components/form/uploadFiles.tsx
@@ -0,0 +1,157 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import FileDisplay from '@/components/ui/file-display'
+import { toast } from '@/components/ui/use-toast'
+import { useFiles, useGeneralForm } from '@/providers/FormProvider'
+import { ArrowUpOnSquareIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Dropzone from 'react-dropzone'
+
+const MAX_FILE_SIZE = 5 * 1024 * 1024 // 5MB
+const acceptedImages = {
+  'image/jpeg': ['.jpeg', '.jpg'],
+  'image/avif': ['.avif'],
+  'image/png': ['.png'],
+  'image/webp': ['.webp'],
+  'application/pdf': ['.pdf'],
+}
+const calculateFileSize = (size: number) => {
+  // Convert bytes to KB, MB, GB, etc.
+  const units = ['bytes', 'KB', 'MB', 'GB', 'TB']
+  let unitIndex = 0
+  let fileSize = size
+  while (fileSize >= 1024 && unitIndex < units.length - 1) {
+    fileSize /= 1024
+    unitIndex++
+  }
+  return `${fileSize.toFixed(2)} ${units[unitIndex]}`
+}
+
+interface Props {
+  fileUploadStep: number
+  uncompleteStep: (step: number) => void
+  completeStep: (step: number) => void
+  setIsDigitalReceiptRequired?: (isRequired: boolean) => void
+}
+
+export default function UploadFiles({
+  fileUploadStep,
+  uncompleteStep,
+  completeStep,
+  setIsDigitalReceiptRequired,
+}: Props) {
+  const { files, removeFile, addFile } = useFiles()
+  const { error, setError } = useGeneralForm()
+
+  return (
+    <>
+      <Dropzone
+        accept={acceptedImages}
+        maxFiles={5}
+        maxSize={MAX_FILE_SIZE}
+        onDropAccepted={(files) => {
+          for (const file of files) {
+            if (file.size > MAX_FILE_SIZE) {
+              setError(
+                `File too large. Max size is ${MAX_FILE_SIZE / 1024 / 1024}MB.`
+              )
+              uncompleteStep(fileUploadStep)
+              return
+            }
+            if (file.name.endsWith('.pdf')) {
+              if (setIsDigitalReceiptRequired) {
+                uncompleteStep(2)
+                setIsDigitalReceiptRequired(true)
+              }
+            }
+            toast({
+              title: 'File uploaded successfully',
+              style: {
+                backgroundColor: 'green',
+                color: 'white',
+                borderRadius: '0.5rem',
+              },
+              description: `File ${file.name} uploaded successfully`,
+              duration: 2000,
+            })
+            addFile(file)
+            completeStep(fileUploadStep)
+            setError('')
+          }
+        }}
+        onDropRejected={(files) => {
+          for (const file of files) {
+            if (file.errors[0].code === 'file-invalid-type') {
+              setError('Invalid file type. Please upload a valid image.')
+            } else if (file.errors[0].code === 'file-too-large') {
+              setError(
+                `File too large. Max size is ${MAX_FILE_SIZE / 1024 / 1024}MB.`
+              )
+            }
+
+            removeFile(file.file)
+
+            if (files.length === 0) {
+              uncompleteStep(fileUploadStep)
+            }
+
+            toast({
+              title: 'File upload failed',
+              style: {
+                backgroundColor: 'red',
+                color: 'white',
+                borderRadius: '0.5rem',
+              },
+              description: `File ${file.file.name} upload failed`,
+              duration: 2000,
+            })
+          }
+        }}
+      >
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()}>
+            {error && <p className='text-red-500'>{error}</p>}
+            <input {...getInputProps()} />
+            <div className='h-96 flex flex-col items-center justify-center gap-2 hover:bg-neutral-200! dark:hover:bg-neutral-800! rounded-md transition-colors cursor-pointer border-2 border-dashed border-neutral-300 dark:border-neutral-700'>
+              <ArrowUpOnSquareIcon className='w-8 h-8' />
+              <p>Drag and drop your file here, or click to select a file.</p>
+            </div>
+          </div>
+        )}
+      </Dropzone>
+      <ul className='flex flex-wrap mt-2 gap-2'>
+        {files && (
+          <FileDisplay files={files}>
+            {(file) => (
+              <Button
+                className='cursor-pointer'
+                size={'icon'}
+                variant={'ghost'}
+                onClick={() => {
+                  removeFile(file)
+                  if (files.length === 1) {
+                    uncompleteStep(fileUploadStep)
+                  }
+                  setError('')
+                  if (file.name.endsWith('.pdf')) {
+                    if (setIsDigitalReceiptRequired) {
+                      setIsDigitalReceiptRequired(false)
+                    }
+                  }
+                  toast({
+                    title: 'File removed',
+                    description: `File ${file.name} removed successfully`,
+                    duration: 2000,
+                  })
+                }}
+                disabled={!file}
+              >
+                <XMarkIcon className='w-4 h-4' />
+              </Button>
+            )}
+          </FileDisplay>
+        )}
+      </ul>
+    </>
+  )
+}
