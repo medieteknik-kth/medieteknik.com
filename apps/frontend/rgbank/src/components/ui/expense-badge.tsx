e07050a92baba8822c8a69b751ab6b0e315c18de
André Eriksson <andreeriksson444@gmail.com>
2025-04-21 02:58:08 +0200

    [frontend] Changed the expense badges

--- apps/frontend/rgbank/src/components/ui/expense-badge.tsx
+++ apps/frontend/rgbank/src/components/ui/expense-badge.tsx
@@ -12,15 +12,17 @@ import {
 
 interface ExpenseBadgeProps {
   type: ExpenseType
+  short?: boolean
   className?: string
 }
 
 interface ExpenseStatusBadgeProps {
   status: ExpenseStatus
+  short?: boolean
   className?: string
 }
 
-export function ExpenseBadge({ type, className }: ExpenseBadgeProps) {
+export function ExpenseBadge({ type, short, className }: ExpenseBadgeProps) {
   switch (type) {
     case 'invoice':
       return (
@@ -29,9 +31,10 @@ export function ExpenseBadge({ type, className }: ExpenseBadgeProps) {
             'w-fit flex items-center gap-2 text-purple-500 bg-purple-500/20 px-2 py-1 rounded-2xl text-xs font-semibold',
             className
           )}
+          title='Invoice'
         >
           <DocumentTextIcon className='h-4 w-4' />
-          Invoice
+          {!short && <span>Invoice</span>}
         </div>
       )
     case 'expense':
@@ -41,9 +44,10 @@ export function ExpenseBadge({ type, className }: ExpenseBadgeProps) {
             'w-fit flex items-center gap-2 text-red-500 bg-red-500/20 px-2 py-1 rounded-2xl text-xs font-semibold',
             className
           )}
+          title='Expense'
         >
           <CreditCardIcon className='h-4 w-4' />
-          Expense
+          {!short && <span>Expense</span>}
         </div>
       )
     default:
@@ -55,6 +59,7 @@ export function ExpenseBadge({ type, className }: ExpenseBadgeProps) {
 
 export function ExpenseStatusBadge({
   status,
+  short,
   className,
 }: ExpenseStatusBadgeProps) {
   switch (status) {
@@ -65,9 +70,10 @@ export function ExpenseStatusBadge({
             'w-fit flex items-center gap-2 text-emerald-500 bg-emerald-500/20 px-2 py-1 rounded-2xl text-xs font-semibold',
             className
           )}
+          title='Booked'
         >
           <ClipboardDocumentCheckIcon className='h-4 w-4' />
-          Booked
+          {!short && <span>Booked</span>}
         </div>
       )
 
@@ -78,9 +84,10 @@ export function ExpenseStatusBadge({
             'w-fit flex items-center gap-2 text-red-500 bg-red-500/20 px-2 py-1 rounded-2xl text-xs font-semibold',
             className
           )}
+          title='Rejected'
         >
           <XMarkIcon className='h-4 w-4' />
-          Rejected
+          {!short && <span>Rejected</span>}
         </div>
       )
 
@@ -91,9 +98,10 @@ export function ExpenseStatusBadge({
             'w-fit flex items-center gap-2 text-blue-500 bg-blue-500/20 px-2 py-1 rounded-2xl text-xs font-semibold',
             className
           )}
+          title='Paid'
         >
           <CreditCardIcon className='h-4 w-4' />
-          Paid
+          {!short && <span>Paid</span>}
         </div>
       )
 
@@ -104,9 +112,10 @@ export function ExpenseStatusBadge({
             'w-fit flex items-center gap-2 text-sky-500 bg-sky-500/20 px-2 py-1 rounded-2xl text-xs font-semibold',
             className
           )}
+          title='Confirmed'
         >
           <CheckIcon className='h-4 w-4' />
-          Confirmed
+          {!short && <span>Confirmed</span>}
         </div>
       )
 
@@ -117,9 +126,10 @@ export function ExpenseStatusBadge({
             'w-fit flex items-center gap-2 text-orange-500 bg-orange-500/20 px-2 py-1 rounded-2xl text-xs font-semibold',
             className
           )}
+          title='Clarification'
         >
           <ExclamationTriangleIcon className='h-4 w-4' />
-          Clarification
+          {!short && <span>Clarification</span>}
         </div>
       )
     case 'UNCONFIRMED':
@@ -129,9 +139,10 @@ export function ExpenseStatusBadge({
             'w-fit flex items-center gap-2 text-yellow-500 bg-yellow-500/20 px-2 py-1 rounded-2xl text-xs font-semibold',
             className
           )}
+          title='Unconfirmed'
         >
           <ClockIcon className='h-4 w-4' />
-          Unconfirmed
+          {!short && <span>Unconfirmed</span>}
         </div>
       )
 
